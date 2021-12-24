2fd258fb9c51df630b5fc2f5ac7a6d6892870232
Aerontian <aeront@hotmail.com>
2021-12-24 10:08:06 +0800

    Push Again 01.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main() {
-    
+
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, push." <<std::endl;
-    std::cout << "Hello, push again." <<std::endl;
+    std::cout << "Hello, push again01." <<std::endl;
 
     return 1;
 }
